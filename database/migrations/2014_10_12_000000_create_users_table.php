672f64745567d284ef3c77fcb2b2b3172ba91460
nyaribo <40204724+nyaribo@users.noreply.github.com>
2018-06-13 14:11:48 +0300

    Update and rename 2014_10_12_000000_create_students_table.php to 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,7 +13,7 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('students', function (Blueprint $table) {
+        Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('student_id')->unique();
             $table->string('fullname');
